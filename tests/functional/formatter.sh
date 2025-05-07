a8721dc177f938abc1af8dc0e9da83e3a64a97ea
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-05-07 11:24:50 -0700

    Expose flake directory to `nix fmt` as `PRJ_ROOT` env var
    This was discussed in https://github.com/NixOS/nix/issues/8034.
    
    I only implemented this for `nix fmt` because it doesn't let you point
    at a flake not on your filesystem.

--- tests/functional/formatter.sh
+++ tests/functional/formatter.sh
@@ -35,12 +35,12 @@ cat << EOF > flake.nix
 EOF
 
 # No arguments check
-[[ "$(nix fmt)" = "Formatting(0):" ]]
-[[ "$(nix formatter run)" = "Formatting(0):" ]]
+[[ "$(nix fmt)" = "PRJ_ROOT=$TEST_HOME Formatting(0):" ]]
+[[ "$(nix formatter run)" = "PRJ_ROOT=$TEST_HOME Formatting(0):" ]]
 
 # Argument forwarding check
-nix fmt ./file ./folder | grep 'Formatting(2): ./file ./folder'
-nix formatter run ./file ./folder | grep 'Formatting(2): ./file ./folder'
+nix fmt ./file ./folder | grep "PRJ_ROOT=$TEST_HOME Formatting(2): ./file ./folder"
+nix formatter run ./file ./folder | grep "PRJ_ROOT=$TEST_HOME Formatting(2): ./file ./folder"
 
 # Build checks
 ## Defaults to a ./result.
